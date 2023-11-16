51af1d2b9b3d67abad989d1b96f1d49711daa39d
mohamed-anish <anishsina1995.14@gmail.com>
2023-11-16 19:44:48 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hi hello!!! welcome to Multiple Stage Form</h1>
+        <div>
+          <label style={{margin:0,padding:0,fontSize:"15px",textAlign:"right"}}>FIRST NAME*</label><br></br>
+          <input type='text' placeholder='Enter your First name' style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
+        </div>
+        <div>
+          <label style={{margin:0,padding:0,fontSize:"15px",textAlign:"right"}}>FIRST NAME*</label><br></br>
+          <input type='text' placeholder='Enter your First name' style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
+        </div>
+        <div>
+          <label style={{margin:0,padding:0,fontSize:"15px",textAlign:"right"}}>FIRST NAME*</label><br></br>
+          <input type='text' placeholder='Enter your First name' style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
+        </div>
+        <div>
+          <label style={{margin:0,padding:0,fontSize:"15px",textAlign:"right"}}>FIRST NAME*</label><br></br>
+          <input type='text' placeholder='Enter your First name' style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
