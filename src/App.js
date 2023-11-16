c805bac9864a49457ba49f5d76a7273c93c7abcc
mohamed-anish <anishsina1995.14@gmail.com>
2023-11-16 19:56:59 +0530

    first commit changes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
           <label style={{margin:0,padding:0,fontSize:"15px",textAlign:"right"}}>FIRST NAME*</label><br></br>
           <input type='text' placeholder='Enter your First name' 
                  style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input><br></br>
-          <button style={{marginTop:"20px"}}>NEXT</button>
+          <button style={{marginTop:"20px",borderRadius:"5px"}}>NEXT</button>
         </div>
 
       </header>
