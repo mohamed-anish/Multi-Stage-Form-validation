3acc89435c4c6de1d07aba50cce44fc5847067fb
mohamed-anish <anishsina1995.14@gmail.com>
2023-11-16 19:53:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ function App() {
         <h1>hi hello!!! welcome to Multiple Stage Form</h1>
         <div>
           <label style={{margin:0,padding:0,fontSize:"15px",textAlign:"right"}}>FIRST NAME*</label><br></br>
-          <input type='text' placeholder='Enter your First name' style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
+          <input type='text' placeholder='Enter your First name' 
+                 style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
         </div>
         <div>
           <label style={{margin:0,padding:0,fontSize:"15px",textAlign:"right"}}>FIRST NAME*</label><br></br>
-          <input type='text' placeholder='Enter your First name' style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
+          <input type='text' placeholder='Enter your First name' 
+                 style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
         </div>
         <div>
           <label style={{margin:0,padding:0,fontSize:"15px",textAlign:"right"}}>FIRST NAME*</label><br></br>
-          <input type='text' placeholder='Enter your First name' style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
+          <input type='text' placeholder='Enter your First name' 
+                 style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
         </div>
         <div>
           <label style={{margin:0,padding:0,fontSize:"15px",textAlign:"right"}}>FIRST NAME*</label><br></br>
-          <input type='text' placeholder='Enter your First name' style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input>
+          <input type='text' placeholder='Enter your First name' 
+                 style={{width:"300px",height:"30px",textAlign:"center",borderRadius:"5px"}}></input><br></br>
+          <button style={{marginTop:"20px"}}>NEXT</button>
         </div>
 
       </header>
